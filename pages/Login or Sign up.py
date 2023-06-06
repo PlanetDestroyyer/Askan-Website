b57a8fea15f424df21897306960807274a6d300d
Pranav <59405252+PlanetDestroyyer@users.noreply.github.com>
2023-06-06 16:36:54 +0530

    Update Login or Sign up.py
    new

--- pages/Login or Sign up.py
+++ pages/Login or Sign up.py
@@ -1,17 +1,7 @@
 import streamlit as st
 import pandas as pd 
 from streamlit_extras.switch_page_button import switch_page
-from pages.Profile import show_profile
-
-session = st.session_state
-def main():
-    st.title("Login Or Resigter")
-    page = st.sidebar.selectbox("Select Page", ("Login", "Registration"))
-
-    if page == "Login":
-        login()
-    elif page == "Registration":
-        signup()
+st.set_page_config(page_title="ASKAN",page_icon="logo.jpg",layout="wide",initial_sidebar_state="auto",menu_items=None)
 
 def append(appendind_data):
     df = pd.DataFrame(appendind_data)
@@ -19,8 +9,10 @@ def append(appendind_data):
 
 df = pd.read_csv('new1.csv')
 
+def profile(name):
+    st.write(name)
 def login():
-    df = pd.read_csv('new1.csv', sep=',')
+    df = pd.read_csv('new1.csv')
     try:
         st.subheader("Login")
         username = st.text_input("Username")
@@ -30,8 +22,7 @@ def login():
         if login_button:
             if username in df['Name'].tolist() and password in df['Password'].tolist():
                 st.success("U have successfully entered into the app")
-                
-                switch_page('Home')
+                profile(username)
             else:
                 st.write("Error")
     except:
@@ -44,7 +35,7 @@ def signup():
         password = st.text_input("Password", type="password")
         confirm_password = st.text_input("Confirm Password", type="password")
         Area_Working = st.number_input("Area_Working:")
-        Country = st.text_input("Country", placeholder="Currently Selected India")
+        Region = st.text_input("Region(India Only)", placeholder="Currently Selected India")
         Gender = st.selectbox("Gender:", ("Male", "Female"))
         Qualification = st.selectbox("Qualification:", ("Educated (10th pass)", "Uneducated"))
         Purpose = st.selectbox("Purpose:", ("Domestic", "Commercial"))
@@ -54,14 +45,28 @@ def signup():
         signup_button = st.button("Register")
         
         if password == confirm_password:
-            st.success("Thanks for Registering")
-            append({"Name": [username], "Password": [password], "Region": [Country], "Area Working": [Area_Working], "Gender": [Gender], "Qualification": [Qualification], "Purpose": [Purpose], "Message": [Message], "Mobile": [Number], "Email": [Email]})
-            session['username'] = username  # Store the registered username
-            show_profile(username)
+            if signup_button:
+                st.success("Thanks for Registering")
+                append({"Name": [username], "Password": [password], "Region": [Region], "Area Working": [Area_Working], "Gender": [Gender], "Qualification": [Qualification], "Purpose": [Purpose], "Message": [Message], "Mobile": [Number], "Email": [Email]})
+                st.write("Go to Login page and Login now")
+            else:
+                pass
         else:
             st.write("Password and Confirm Password should be the same")
     except:
         pass
+
+
+
+session = st.session_state
+def main():
+    st.title("Login Or Resigter")
+    page = st.sidebar.selectbox("Select Page", ("Login", "Registration"))
+
+    if page == "Login":
+        login()
+    elif page == "Registration":
+        signup()
 
 def hideAll():
     hide = """
@@ -75,6 +80,9 @@ def hideAll():
 
 hideAll()
 
-if __name__ == "__main__":
-    main()                                                                                                                       
-    
+def style():
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+style()
+main()
