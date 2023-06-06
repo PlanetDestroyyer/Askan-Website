062e8953139562eb2d1126d12e36b02457a28f78
wrieddude <59405252+PlanetDestroyyer@users.noreply.github.com>
2023-06-06 07:47:25 +0530

    login data
    new

--- pages/Login or Sign up.py
+++ pages/Login or Sign up.py
@@ -1,22 +1,23 @@
 import streamlit as st
 import pandas as pd 
 from streamlit_extras.switch_page_button import switch_page
-def main():
-    st.title("Login and Sign-up")
+from pages.Profile import show_profile
 
-    # Page selection
-    page = st.sidebar.selectbox("Select Page", ("Login", "Sign-up"))
+session = st.session_state
+def main():
+    st.title("Login Or Resigter")
+    page = st.sidebar.selectbox("Select Page", ("Login", "Registration"))
 
     if page == "Login":
         login()
-    elif page == "Sign-up":
+    elif page == "Registration":
         signup()
 
 def append(appendind_data):
     df = pd.DataFrame(appendind_data)
     df.to_csv('new1.csv', mode='a', index=False, header=False)
 
-# df = pd.read_csv('new1.csv')
+df = pd.read_csv('new1.csv')
 
 def login():
     df = pd.read_csv('new1.csv', sep=',')
@@ -27,10 +28,9 @@ def login():
         login_button = st.button("Login")
 
         if login_button:
-            # userList = 
-            # print(df['Name'].tolist())
             if username in df['Name'].tolist() and password in df['Password'].tolist():
                 st.success("U have successfully entered into the app")
+                
                 switch_page('Home')
             else:
                 st.write("Error")
@@ -39,20 +39,27 @@ def login():
 
 def signup():
     try:
-        st.subheader("Sign-up")
+        st.subheader("Registration")
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
         confirm_password = st.text_input("Confirm Password", type="password")
-        signup_button = st.button("Sign-up")
+        Area_Working = st.number_input("Area_Working:")
+        Country = st.text_input("Country", placeholder="Currently Selected India")
+        Gender = st.selectbox("Gender:", ("Male", "Female"))
+        Qualification = st.selectbox("Qualification:", ("Educated (10th pass)", "Uneducated"))
+        Purpose = st.selectbox("Purpose:", ("Domestic", "Commercial"))
+        Message = st.selectbox("Allow Personalized Message:", ("Yes", "No"))
+        Number = st.text_input("Mobile Number :")
+        Email = st.text_input("Email :")
+        signup_button = st.button("Register")
+        
         if password == confirm_password:
-            if signup_button:
-                st.success("Thanks for Signing")
-                append({"Name": [username],"Password":[password]})
-                switch_page('Home')
-            else:
-                st.write("Please fill form")
+            st.success("Thanks for Registering")
+            append({"Name": [username], "Password": [password], "Region": [Country], "Area Working": [Area_Working], "Gender": [Gender], "Qualification": [Qualification], "Purpose": [Purpose], "Message": [Message], "Mobile": [Number], "Email": [Email]})
+            session['username'] = username  # Store the registered username
+            show_profile(username)
         else:
-            st.write("Password and Confirm Password should be same")
+            st.write("Password and Confirm Password should be the same")
     except:
         pass
 
--- pages/Profile.py
+++ pages/Profile.py
@@ -1,117 +1,20 @@
+# # profile.py
+
 import streamlit as st
-import pandas as pd
+
+def show_profile(username):
+    if username in df['Name'].tolist() and password in df['Password'].tolist():
+        # Retrieve user's data based on the username
+        # Display the profile information
+        st.write("Username:", username)
+        # Add more fields as necessary
 
 
-df = pd.read_csv('new1.csv')
+def profile():
+    show_profile(session['username'])
 
-def main():
-    st.title("Profile Page")
-    st.subheader("User Profile")
-    st.session_state = False
-    if st.session_state == False:
-        name = st.text_input("Name:")
-        email = st.text_input("Email:")
-        phone_number = st.text_input("Mobile Number:")
-        gender = st.selectbox("Gender:", ("Male", "Female"))
-        qualification = st.selectbox("Qualification:", ("Educated (10th pass)", "Uneducated"))
-        purpose = st.selectbox("Purpose:", ("Domestic", "Commercial"))
-        allow_personalized_message = st.selectbox("Allow Personalized Message:", ("Yes", "No"))
-    else:
-        # df['Name'].tolist():
+if username in df['Name'].tolist() and password in df['Password'].tolist():
 
-        name = df['Name']
-        email = df['Email']
-        phone_number = df['Mobile Number']
-        gender = df['Name']
-        qualification = df['Email']
-        purpose = df["Email"]
-        allow_personalized_message =df["Email"]
-
-    if st.button("Save"):
-        # Perform save action here
-        st.write("Information saved successfully.")
-        st.session_state.editable = False
-
-    # if st.button("Edit"):
-    #     st.session_state.editable = True
-
-    # # Render the profile information as read-only if not editable
-    # if not st.session_state.editable:
-    #     st.text_input("Name:", value=name, key="name", disabled=True)
-    #     st.text_input("Email:", value=email, key="email", disabled=True)
-    #     st.text_input("Phone Number:", value=phone_number, key="phone_number", disabled=True)
-    #     st.selectbox("Gender:", ("Male", "Female"), index=0 if gender == "Male" else 1, key="gender", disabled=True)
-    #     st.selectbox("Qualification:", ("Educated (10th pass)", "Uneducated"), index=0 if qualification == "Educated (10th pass)" else 1, key="qualification", disabled=True)
-    #     st.selectbox("Purpose:", ("Domestic", "Commercial"), index=0 if purpose == "Domestic" else 1, key="purpose", disabled=True)
-    #     st.selectbox("Allow Personalized Message:", ("Yes", "No"), index=0 if allow_personalized_message == "Yes" else 1, key="allow_personalized_message", disabled=True)
-
-    if st.button("Logout"):
-        pass
-        # Perform logout action here
-        # session_state = st.session_state
-        # if "logged_in" not in session_state:
-        #     session_state["logged_in"] = False
-    
-        # if session_state["logged_in"]:
-        #     # Display the main content for logged-in users
-        #     st.title("Welcome to the App")
-        #     # Your app content goes here
-    
-        #     # Logout button
-        #     if st.button("Logout"):
-        #         logout()
-        # else:
-        #     # Display the login form if the user is not logged in
-        #     st.title("Login")
-        #     username = st.text_input("Username")
-        #     password = st.text_input("Password", type="password")
-            
-        #     if username == "your_username" and password == "your_password":
-        #         # Set session state to logged-in
-        #         session_state["logged_in"] = True
-        #         st.success("Logged in successfully!")
-        #     else:
-        #         st.error("Invalid username or password")
-
-
-def style():
-    with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-def hide_all():
-    hide = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-        </style>
-        """   
-    st.markdown(hide, unsafe_allow_html=True)
-
-def remove_underline():
-    st.markdown(
-        """
-        <style>
-        a {
-            text-decoration: none;
-            color : black;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-def app():
-    hide_all()
-    remove_underline()
-    style()
-    # if "editable" not in st.session_state:
-    #     st.session_state.editable = True
-    main()
-
-if __name__ == '__main__':
-    app()
-
-
-    
-
+    session['username'] = name  # Store the logged-in username
+    st.success("You have successfully entered into the app")
+    show_profile(name)
